c2935c5951f30159512b8d47b8fe27975ba46bd5
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2022-05-31 11:02:09 +0100

    change port to general port

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,6 @@
-
-
-$("#add_user").submit((event)=>{
-  alert("Data Inserted Successfully!")
-})
+$("#add_user").submit((event) => {
+  alert("Data Inserted Successfully!");
+});
 
 // function selectObj(obj){
 //   let element = document.querySelector(obj)
@@ -13,48 +11,42 @@ $("#add_user").submit((event)=>{
 //   alert('Data Inserted Successfully!')
 // })
 
-$("#update_user").submit(function(event){
-  event.preventDefault()
+$("#update_user").submit(function (event) {
+  event.preventDefault();
 
-  var unindexed_array = $(this).serializeArray()
-  var data = {}
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
 
-  $.map(unindexed_array,(n,i)=>{
-    data[n['name']] = n['value']
-  })
-
-  
- 
-
-
+  $.map(unindexed_array, (n, i) => {
+    data[n["name"]] = n["value"];
+  });
 
   var request = {
-    "url" : `http://localhost:3000/api/users/${data.id}`,
-    "method" : "PUT",
-    "data" : data
-  }
+    url: `http://localhost:process.env.PORT/api/users/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
 
-  $.ajax(request).done((response)=>{
-    alert("Data Updated Successfully!")
-  })
-})
+  $.ajax(request).done((response) => {
+    alert("Data Updated Successfully!");
+  });
+});
 
-if(window.location.pathname == "/"){
- 
-  $ondelete = $(".table tbody td a.delete")
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
 
-  $ondelete.click(function(){
-    var id = $(this).attr("data-id")
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
     var request = {
-      "url" : `http://localhost:3000/api/users/${id}`,
-      "method" : "DELETE",
-    }
+      url: `http://localhost:process.env.PORT/api/users/${id}`,
+      method: "DELETE",
+    };
 
-    if(confirm("Do you really want to delete this record?")){
-      $.ajax(request).done((response)=>{
-        alert("Data Deleted Successfully!")
-        location.reload()
-      })
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done((response) => {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
     }
-  })
+  });
 }
